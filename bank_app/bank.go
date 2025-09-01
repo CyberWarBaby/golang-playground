051ff352779c2ae67419a402f6e496a0ccbbd925
cyberwarbaby <habeebjimoh29@gmail.com>
2025-09-01 13:46:54 +0100

    Added If Else statement so now the banking app takes user input and process it

--- bank_app/bank.go
+++ bank_app/bank.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+
+	var accbalance float64 = 1000
+
 	fmt.Println("Welcome to CYPH-GO Bank")
 	fmt.Println("What will you like to do")
 	fmt.Println("1. Check balance")
@@ -10,11 +13,26 @@ func main() {
 	fmt.Println("3. Withdraw money")
 	fmt.Println("4. Exit")
 
-	var choice int 
-	fmt.Print("Your choice?")
+	var choice int
+	fmt.Print("Your choice? ")
 	fmt.Scan(&choice)
 
-	if choice == 1 
+	var amount float64
 
-	fmt.Println("Your choice:", choice)
+	if choice == 1 {
+		fmt.Println("Your balance is:", accbalance)
+	} else if choice == 2 {
+		fmt.Print("Amount you want to deposit: ")
+		fmt.Scan(&amount)
+		accbalance += amount
+		fmt.Println("Your new account balance is:", accbalance)
+	} else if choice == 3 {
+		fmt.Print("Amount you want to withdraw: ")
+		fmt.Scan(&amount)
+		accbalance -= amount
+		fmt.Println("New account balance:", accbalance)
+	} else {
+		fmt.Println("Goodbye and stay safe!!")
+	}
+
 }
