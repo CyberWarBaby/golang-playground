7bc7f2561d554f9549e6da3fadbc61384ae3bfaf
cyberwarbaby <habeebjimoh29@gmail.com>
2025-09-03 14:37:38 +0100

    Learnt about the break and return key words and continue

--- bank_app/bank.go
+++ bank_app/bank.go
@@ -7,44 +7,50 @@ func main() {
 	var accbalance float64 = 1000
 
 	fmt.Println("Welcome to CYPH-GO Bank")
-	fmt.Println("What will you like to do")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Your choice? ")
-	fmt.Scan(&choice)
-
-	var amount float64
-
-	if choice == 1 {
-		fmt.Println("Your balance is:", accbalance)
-	} else if choice == 2 {
-		fmt.Print("Amount you want to deposit: ")
-		fmt.Scan(&amount)
-
-		if amount <= 0 {
-			fmt.Print("Please enter a valid amount!")
-			return
+	for {
+		fmt.Println("What will you like to do")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+	
+		var choice int
+		fmt.Print("Your choice? ")
+		fmt.Scan(&choice)
+	
+		var amount float64
+	
+		if choice == 1 {
+			fmt.Println("Your balance is:", accbalance)
+		} else if choice == 2 {
+			fmt.Print("Amount you want to deposit: ")
+			fmt.Scan(&amount)
+	
+			if amount <= 0 {
+				fmt.Println("Please enter a valid amount!")
+				continue
+			}
+	
+			accbalance += amount
+			fmt.Println("Your new account balance is:", accbalance)
+		} else if choice == 3 {
+			fmt.Print("Amount you want to withdraw: ")
+	
+			if amount > accbalance {
+				fmt.Print("SORRY! Insufficient balance :( ")
+				return
+			}
+	
+			fmt.Scan(&amount)
+			accbalance -= amount
+			fmt.Println("New account balance:", accbalance)
+		} else {
+			fmt.Println("Goodbye and stay safe!!")
+			break
 		}
 
-		accbalance += amount
-		fmt.Println("Your new account balance is:", accbalance)
-	} else if choice == 3 {
-		fmt.Print("Amount you want to withdraw: ")
-
-		if amount > accbalance {
-			fmt.Print("SORRY! Insufficient balance :( ")
-			return
-		}
-
-		fmt.Scan(&amount)
-		accbalance -= amount
-		fmt.Println("New account balance:", accbalance)
-	} else {
-		fmt.Println("Goodbye and stay safe!!")
+		fmt.Println("Thanks for choosing us")
+	
 	}
-
 }
