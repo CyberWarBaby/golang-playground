1e6459907ce44e13fa42392706f0a6c5075b901f
cyberwarbaby <habeebjimoh29@gmail.com>
2025-10-20 11:24:39 +0100

    Started building the investment calculator again from scratch

---
+++ investment_calculator/investment_calculator.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 6.5
+	fmt.Println("Hello investment")
+	var investmentAmount, years float64 = 1000, 10
+	expectedReturnRate := 5.5
+
+	fmt.Scan(&investmentAmount)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, (years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
