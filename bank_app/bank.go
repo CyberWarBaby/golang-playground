41d9d6cfda777cb84e0388a011d9ec17e843bf28
cyberwarbaby <habeebjimoh29@gmail.com>
2025-10-21 12:34:24 +0100

    started working with working on the bank app with if else statements

---
+++ bank_app/bank.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	var balance float64 = 1000
+
+	fmt.Println("Welcome to GO Bank!")
+	fmt.Println("What do yo want to do?")
+	fmt.Println("1. check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var userInput int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&userInput)
+
+	if userInput == 1 {
+
+		fmt.Println(`Your balance is`, balance)
+
+	} else if userInput == 2 {
+
+		fmt.Print("Deposit amount: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		balance += depositAmount
+		fmt.Println("New balance is ", balance)
+
+	} else if userInput == 3 {
+
+		fmt.Print("Withdrawal amount: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+		balance -= withdrawAmount
+		fmt.Println("New balance is ", balance)
+
+	} else {
+		println("bye")
+	}
+
+}
