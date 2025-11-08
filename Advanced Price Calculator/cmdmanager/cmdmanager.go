24f85cb5f1ca704b58f9ffbaa06b23563f246286
cyberwarbaby <habeebjimoh29@gmail.com>
2025-11-08 12:48:15 +0000

    Working on a Swappable Struct

---
+++ Advanced Price Calculator/cmdmanager/cmdmanager.go
@@ -0,0 +1,37 @@
+package cmdmanager
+
+import (
+	"fmt"
+)
+
+type CMDManager struct {
+}
+
+func (cmd CMDManager) ReadLines() ([]string, error) {
+
+	fmt.Println("Pls enter your prices with enter")
+
+	var prices []string
+
+	for {
+		var price string
+		fmt.Println("Price: ")
+		fmt.Scan(&price)
+
+		if price == "0" {
+			break
+		}
+		prices = append(prices, price)
+	}
+
+	return prices, nil
+}
+
+func (cmd CMDManager) WriteResult(data interface{}) error {
+	fmt.Println(data)
+	return nil
+}
+
+func New() CMDManager {
+	return CMDManager{}
+}
